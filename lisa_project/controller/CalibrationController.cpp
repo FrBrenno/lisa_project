a5bf8515cf289f7221ab54a173322d5b858b3e7b
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2024-03-24 20:43:01 +0100

    Calibration Algorithm Implementated and working.

--- lisa_project/controller/CalibrationController.cpp
+++ lisa_project/controller/CalibrationController.cpp
@@ -10,7 +10,7 @@ CalibrationController::CalibrationController() {
 	this->gaussKernel = Size(15, 15);
 	this->blockSize = 31;
 	this->c = 3;
-	this->clusterDistance = 10;
+	this->clusterDistance = 20;
 
 	EventDispatcher::Instance().SubscribeToEvent<CalibrationStartEvent>(
 		[this](const CalibrationStartEvent& event) {
@@ -25,7 +25,7 @@ void CalibrationController::HandleCalibrationStart()
 	// apply the calibration pipeline to the frame	
 }
 
-void CalibrationController::setParameters(const cv::Size& gaussKernel, int blockSize, double c, double clusterDistance) {
+void CalibrationController::setParameters(const Size& gaussKernel, int blockSize, double c, double clusterDistance) {
 	this->gaussKernel = gaussKernel;
 	this->blockSize = blockSize;
 	this->c = c;
@@ -97,15 +97,16 @@ std::vector<double> CalibrationController::getPeaks(const std::vector<int>& inte
     return peaks;
 }
 
-std::vector<cv::Point2d> CalibrationController::getCircles(const Mat& image) {
+std::vector<Point2d> CalibrationController::getCircles(const Mat& image) {
     std::vector<int> intensityX = intensityHist(image);
     std::vector<double> peaksX = getPeaks(intensityX);
     std::vector<int> intensityY = intensityHist(image.t());
     std::vector<double> peaksY = getPeaks(intensityY);
 
-    std::vector<cv::Point2d> circles;
-    for (double x : peaksX) {
-        for (double y : peaksY) {
+    std::vector<Point2d> circles;
+    // Generate all possible circles and sorting them by y-coordinate
+    for (double y : peaksY) {
+        for (double x : peaksX) {
             circles.push_back(Point2d(x, y));
         }
     }
@@ -147,26 +148,26 @@ void displayMatrix(const Eigen::MatrixXd& A, int startRow = 0) {
     int cols = A.cols();
 
     // Create an OpenCV Mat to store the printed matrix
-    cv::Mat matrixImage(rows * 20, cols * 60, CV_8UC3, cv::Scalar(255, 255, 255)); // Adjust size as needed
+    Mat matrixImage(rows * 20, cols * 60, CV_8UC3, Scalar(255, 255, 255)); // Adjust size as needed
 
     // Print the matrix onto the OpenCV Mat
     for (int i = startRow; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             std::stringstream ss;
             ss << std::fixed << std::setprecision(2) << A(i, j);
-            cv::putText(matrixImage, ss.str(), cv::Point(j * 60, (i - startRow + 1) * 20), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 0), 1);
+            putText(matrixImage, ss.str(), Point(j * 60, (i - startRow + 1) * 20), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 0), 1);
 
         }
     }
 
     // Display the OpenCV Mat in a window
-    cv::imshow("Matrix Viewer", matrixImage);
-    cv::waitKey(0); // Wait for a key press
+    imshow("Matrix Viewer", matrixImage);
+    waitKey(0); // Wait for a key press
 }
 
-CalibrationData* CalibrationController::applyCalibrationPipeline(const cv::Mat& image){
-    cv::Mat thresh = generateThresholdImg(image);
-    std::vector<cv::Point2d> circles = getCircles(thresh);
+CalibrationData* CalibrationController::applyCalibrationPipeline(const Mat& image){
+    Mat thresh = generateThresholdImg(image);
+    std::vector<Point2d> circles = getCircles(thresh);
 
     int numCircles = circles.size();
     initializeMatrixA(numCircles);
@@ -183,33 +184,32 @@ CalibrationData* CalibrationController::applyCalibrationPipeline(const cv::Mat& 
     // Compute the least square solution of the system
     // X = (cx0, cy0, dx, dy)
     Eigen::MatrixXd X = svd.solve(B);
-    displayMatrix(A);
-    displayMatrix(A, numCircles);
-    displayMatrix(B);
-    displayMatrix(B, numCircles);
-    displayMatrix(X);
-    cv::Mat outputImage = image.clone();
+    Mat outputImage = image.clone();
     // Draw circles of diameter means(X(2), X(3)) at the computed positions
-    int radius = (int)(X(2) + X(3)) / 2;
-    for (const auto& circle : circles) {
-		cv::circle(outputImage, cv::Point(circle.x, circle.y), radius, cv::Scalar(0, 0, 255), 1);
+    int radius = (int)(X(2) + X(3)) / 4;
+    int cx0 = (int)X(0);
+    int cy0 = (int)X(1);
+    for (const auto& c : circles) {
+		cv::circle(outputImage, Point((int)(c.x), (int)(c.y)), radius, Scalar(0, 0, 255), 1);
 	}
 
-    // Draw grid lines
-    for (int i = 0; i < numCircles; ++i) {
-		cv::line(outputImage, cv::Point(0, (int)(X(3) * i)), cv::Point(outputImage.cols, (int)(X(3) * i)), cv::Scalar(0, 255, 0), 1);
-		cv::line(outputImage, cv::Point((int)(X(2) * i), 0), cv::Point((int)(X(2) * i), outputImage.rows), cv::Scalar(0, 255, 0), 1);
-	}
+    // Add grid lines
+    for (int i = X(2); i < outputImage.rows; i += X(2)) {
+        line(outputImage, Point(i, 0), Point(i, outputImage.cols), Scalar(0,0,255));
+    }
+    for (int j = X(3); j < outputImage.cols; j += X(3)) {
+        line(outputImage, Point(0, j), Point(outputImage.rows, j), Scalar(0, 0, 255));
+    }
     // Draw box with all calibration parameters & results
-    cv::rectangle(outputImage, cv::Point(0, 0), cv::Point(200, 180), cv::Scalar(0, 0, 0), -1);
-    cv::putText(outputImage, "c: " + std::to_string(c), cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "blockSize: " + std::to_string(blockSize), cv::Point(10, 40), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "gaussKernel: " + std::to_string(gaussKernel.width) + "x" + std::to_string(gaussKernel.height), cv::Point(10, 60), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "clusterDistance: " + std::to_string((int)clusterDistance), cv::Point(10, 80), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "dx: " + std::to_string(X(2)), cv::Point(10, 100), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "dy: " + std::to_string(X(3)), cv::Point(10, 120), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "cx0: " + std::to_string(X(0)), cv::Point(10, 140), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
-    cv::putText(outputImage, "cy0: " + std::to_string(X(1)), cv::Point(10, 160), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1);
+    rectangle(outputImage, Point(0, 0), Point(200, 180), Scalar(0, 0, 0), -1);
+    putText(outputImage, "c: " + std::to_string(c), Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "blockSize: " + std::to_string(blockSize), Point(10, 40), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "gaussKernel: " + std::to_string(gaussKernel.width) + "x" + std::to_string(gaussKernel.height), Point(10, 60), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "clusterDistance: " + std::to_string((int)clusterDistance), Point(10, 80), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "dx: " + std::to_string(X(2)), Point(10, 100), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "dy: " + std::to_string(X(3)), Point(10, 120), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "cx0: " + std::to_string(X(0)), Point(10, 140), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+    putText(outputImage, "cy0: " + std::to_string(X(1)), Point(10, 160), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
 
     return new CalibrationData(outputImage, X(2), X(3), circles);
 }
